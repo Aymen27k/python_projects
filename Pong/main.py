055146dd973245a56742da8f9d195cf0c2e3d79a
Aymen Kalaï Ezar <aymen27k@gmail.com>
2025-06-03 17:11:31 +0100

    feat: added a win condition and player names

--- Pong/main.py
+++ Pong/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from scoreboard import Scores
 from paddle import Paddle
 from ball import Ball
@@ -8,30 +8,40 @@ import time
 def main():
     # Main screen initialization
     screen = Screen()
-    screen.listen()
+
     screen.tracer(0)
     screen.setup(width=900, height=600)
     screen.bgcolor("black")
     screen.title("Pong | Made by Aymen")
-    score = Scores()
+    # Players name inputs
+    first_player = screen.textinput(title="Player 1",
+                                     prompt="Enter left player name : ")
+    second_player = screen.textinput(title="Player 2",
+                                     prompt="Enter right player name : ")
+    l_player_name = first_player.capitalize()
+    r_player_name = second_player.capitalize()
+    score = Scores(l_player_name, r_player_name)
     score.draw_the_net()
     score.score_display()
     ball = Ball()
 
     left_paddle = Paddle(-420)
     right_paddle = Paddle(420)
+    screen.listen()
     game_is_on = True
     while game_is_on:
 
         ball.ball_motion()
+
         # Detect collision with wall
         if ball.ycor() > 280 or ball.ycor() < - 270:
             ball.bounce_y()
+
         # Detect collision with the paddles
         if ball.distance(right_paddle) < 20 and ball.xcor() > 395 or ball.distance(
-                left_paddle) < 20 and ball.xcor() < - 410:
+                left_paddle) < 20 and ball.xcor() < - 395:
             ball.bounce_x()
-            print(ball.move_speed)
+
         # Detect ball goes beyond the right limits
         if ball.xcor() > 460:
             score.l_point()
@@ -41,7 +51,15 @@ def main():
         if ball.xcor() < -460:
             score.r_point()
             ball.reset_ball()
-
+        # Win condition
+        if score.l_score == 7:
+            score.winner(l_player_name)
+            ball.hideturtle()
+            game_is_on = False
+        elif score.r_score == 7:
+            score.winner(r_player_name)
+            ball.hideturtle()
+            game_is_on = False
         # Movement trigger
         # Left Paddle
         screen.onkey(left_paddle.move_up, "z")
--- Pong/scoreboard.py
+++ Pong/scoreboard.py
@@ -5,7 +5,7 @@ FONT = ("arial", 28, "normal")
 
 
 class Scores(Turtle):
-    def __init__(self):
+    def __init__(self, l_player, r_player):
         super().__init__()
         self.penup()
         self.hideturtle()
@@ -13,6 +13,8 @@ class Scores(Turtle):
         self.speed("fastest")
         self.l_score = 0
         self.r_score = 0
+        self.l_player = l_player
+        self.r_player = r_player
 
     def draw_the_net(self):
         self.goto(0, 300)
@@ -26,9 +28,9 @@ class Scores(Turtle):
     def score_display(self):
         self.draw_the_net()
         self.goto(-225, 250)
-        self.write(self.l_score, False, ALIGNMENT, FONT)
+        self.write(f"{self.l_player} : {self.l_score}", False, ALIGNMENT, FONT)
         self.goto(225, 250)
-        self.write(self.r_score, False, ALIGNMENT, FONT)
+        self.write(f"{self.r_player} : {self.r_score}", False, ALIGNMENT, FONT)
 
     def l_point(self):
         self.clear()
@@ -39,3 +41,8 @@ class Scores(Turtle):
         self.clear()
         self.r_score += 1
         self.score_display()
+
+    def winner(self, player):
+        self.clear()
+        self.goto(0, 0)
+        self.write(f"{player} Won", False, ALIGNMENT, FONT)
